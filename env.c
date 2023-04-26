533d23060f0cbfb95195c32cf4ba54b814d356aa
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-04-26 17:49:27 +0000

    commit

--- env.c
+++ env.c
@@ -1,19 +1,46 @@
 #include "shell.h"
 
 /**
- * _getenv - returns the string array copy of our env
+ * _getenv - Gets an env variable.
  *@info: Structure containing potential arguments.
-
- * Return: Always 0
+ * 
+ * Return: NULL.
  */
 
-char **_getenv(info_t *info)
+char **_get_env(char *var)
 {
-	if (!info->environ || info->env_changed)
+	int i, len;
+
+	len = _strlen(var);
+	for (i = 0; env[i]; i++)
 	{
-		info->_env = list_to_strings(info->_env);
-		info->env_changed = 0;
+		if (_strncmp(var, env[i], len) == 0)
+			return (&env[i]);
 	}
 
-	return (info->_env);
+	return (NULL);
+}
+
+list_s *_get_env_value(char *variable)
+{
+	int equal_sign_index = 0;
+	int index = 0;
+	char *dils = NULL;
+	char **env_value = _get_env(var);
+	list_s *result = NULL;
+
+	if (!env_value)
+		return (NULL);
+
+	while ((*env_value)[i] != '=')
+		i++;
+	equal_sign_index = ++i;
+
+	dils = _strtok((*env_value) + equal_sign_index, ":");
+	while (dils)
+	{
+		add_node_end(&result, dils);
+		dils = _strtok(NULL, ":");
+	}
+	return (result);
 }
